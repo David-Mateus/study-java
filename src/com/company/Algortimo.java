cbcc3dcc9e34d1b31934fa6aacbd580bdda8bbd8
David-Mateus <david-mateus09@hotmail.com>
2023-10-09 18:24:59 -0300

    study java

---
+++ src/com/company/Algortimo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Algortimo {
+    public static void main(String[] args) {
+        Acc vector = new Acc(5);
+        vector.addLastVector("David");
+        vector.addLastVector("David");
+        vector.addLastVector("David");
+
+
+        System.out.println(vector.searchElement("David"));
+        vector.addLastVector(2);
+        System.out.println(vector.toString());
+    }
+}
---
+++ src/com/company/Herança.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Herança {
+    public static void main(String[] args){
+        BusinnesAccount account = new BusinnesAccount();
+
+    }
+}
